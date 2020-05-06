0f3682bd42f8ca3737638dda4839c16cbd46ee52
StrelnikovAlex <strelnikov.spb@gmail.com>
2020-05-06 16:54:39 +0300

    fix code

--- app/code/Shellpea/None/Plugin/CustomBreadcrumbs.php
+++ app/code/Shellpea/None/Plugin/CustomBreadcrumbs.php
@@ -5,13 +5,6 @@ namespace Shellpea\None\Plugin;
 class CustomBreadcrumbs
 {
 
-    /**
-     * @param \Magento\Theme\Block\Html\Breadcrumbs $subject
-     * @param string $crumbName
-     * @param array $crumbInfo
-     * @return array
-     */
-
     public function beforeAddCrumb(\Magento\Theme\Block\Html\Breadcrumbs $subject, $crumbName, $crumbInfo) {
 
         $crumbInfo['label'] = $crumbInfo['label'].'(!)';
