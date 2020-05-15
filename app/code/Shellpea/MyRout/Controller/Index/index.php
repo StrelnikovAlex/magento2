e82f4c645ce149c9916c1a4c2d2ecc62a55d9078
StrelnikovAlex <strelnikov.spb@gmail.com>
2020-05-15 11:37:52 +0300

    Changed controller made yesterday

--- app/code/Shellpea/MyRout/Controller/Index/index.php
+++ app/code/Shellpea/MyRout/Controller/Index/index.php
@@ -5,24 +5,28 @@ namespace Shellpea\MyRout\Controller\Index;
 use \Magento\Framework\App\Action\Action;
 use \Magento\Framework\Controller\Result\RawFactory;
 use \Magento\Framework\App\Action\Context;
+use \Magento\Framework\Controller\Result\Redirect;
 
 
 class Index extends Action
 {
   protected $resultRawFactory;
 
+  protected $resultRedirect;
+
   public function __construct(
     Context $context,
-    RawFactory $resultRawFactory
-    )
+    RawFactory $resultRawFactory,
+    Redirect $resultRedirect)
     {
       $this->resultRawFactory = $resultRawFactory;
+      $this->resultRedirect = $resultRedirect;
       return parent::__construct($context);
     }
 
     public function execute()
     {
-      return $this->resultRawFactory->create()->setContents('НАКОНЕЦ-ТО ЭТО ВОНюЧИЙ HELLO WORLD ЗАРАБОТАЛ!!!!!!!!!!!!!!!!!!11');
-
+    //  return $this->resultRawFactory->create()->setContents('НАКОНЕЦ-ТО ЭТО ВОНюЧИЙ HELLO WORLD ЗАРАБОТАЛ!!!!!!!!!!!!!!!!!!11');
+      return $this->resultRedirect->setPath('/');
     }
   }
