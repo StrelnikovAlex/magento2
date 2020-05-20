c410f56b8c4768f5ffc9a1681fe5df786cdd444f
StrelnikovAlex <strelnikov.spb@gmail.com>
2020-05-20 11:38:36 +0300

    myblock

---
+++ app/code/Shellpea/MyBlock/Block/Info.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shellpea\MyBlock\Block;
+
+use Magento\Framework\View\Element\AbstractBlock;
+
+class Info extends AbstractBlock
+{
+    protected function _toHtml()
+    {
+        return 'join my page';
+    }
+}
---
+++ app/code/Shellpea/MyBlock/Controller/Index/Info.php
@@ -0,0 +1,24 @@
+<?php
+namespace Shellpea\MyBlock\Controller\Index;
+
+class Info extends \Magento\Framework\App\Action\Action
+{
+    protected $_resultPageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->_resultPageFactory = $resultPageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $html = $this->_resultPageFactory->create()
+                                         ->getLayout()
+                                         ->createBlock('Shellpea\MyBlock\Block\Info')
+                                         ->toHtml();
+        $this->getResponse()->setBody($html);
+    }
+}
